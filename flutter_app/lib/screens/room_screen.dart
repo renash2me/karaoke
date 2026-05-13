b651050c48e7d2af757f2052121fa5f0b937d92a
rezambelli <122014704+renash2me@users.noreply.github.com>
2026-05-13 14:54:14 -0300

    Fix navigation path in _sing function

--- flutter_app/lib/screens/room_screen.dart
+++ flutter_app/lib/screens/room_screen.dart
@@ -100,7 +100,7 @@ class _RoomScreenState extends State<RoomScreen>
   }
 
   Future<void> _sing(Map<String, dynamic> song) async {
-    context.go('/play/${widget.roomId}/${song['id']}', extra: {
+    context.go('/room/${widget.roomId}/play/$songId',${widget.roomId}/${song['id']}', extra: {
       'song': song,
       'singerName': widget.singerName,
     });
